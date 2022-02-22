dd156ae1b9d84403a61ba51e2621822e554d6409
catsout <outline941@live.com>
2022-02-22 14:51:45 +0800

    scene: vulkan init(wip)

---
+++ src/backend_scene/src/wallpaper/Particle/ParticleSystem.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "ParticleEmitter.h"
+#include "Scene/SceneMesh.h"
+#include "Interface/IParticleRawGener.h"
+#include "Utils/NoCopyMove.hpp"
+
+#include <memory>
+
+namespace wallpaper {
+
+enum class ParticleAnimationMode {
+	SEQUENCE,
+	RANDOMONE
+};
+
+class ParticleSystem;
+
+class ParticleSubSystem : NoCopy,NoMove {
+public:
+	ParticleSubSystem(ParticleSystem& p, 
+		std::shared_ptr<SceneMesh> sm, 
+		uint32_t maxcount, 
+		float rate, 
+		ParticleRawGenSpecOp specOp)
+		:parent(p),m_mesh(sm),m_maxcount(maxcount),m_rate(rate),m_genSpecOp(specOp) {};
+
+	void Emitt();
+
+	void AddEmitter(ParticleEmittOp&&);
+	void AddInitializer(ParticleInitOp&&);
+	void AddOperator(ParticleOperatorOp&&);
+private:
+	ParticleSystem& parent;
+	std::shared_ptr<SceneMesh> m_mesh;
+//	std::vector<std::unique_ptr<ParticleEmitter>> m_emiters;
+	std::vector<ParticleEmittOp> m_emiters;
+
+	std::vector<Particle> m_particles;
+	std::vector<ParticleInitOp> m_initializers;
+	std::vector<ParticleOperatorOp> m_operators;
+	ParticleRawGenSpecOp m_genSpecOp;
+	uint32_t m_maxcount;
+	float m_rate;
+};
+
+class Scene;
+class ParticleSystem : NoCopy,NoMove {
+public:
+	ParticleSystem(Scene& scene):scene(scene) {};
+	~ParticleSystem() = default;
+
+	void Emitt();
+
+	Scene& scene;
+
+	std::vector<std::unique_ptr<ParticleSubSystem>> subsystems;
+	std::unique_ptr<IParticleRawGener> gener;
+};
+}
---
+++ src/backend_scene/src/wallpaper/Scene/SceneShader.cpp
@@ -0,0 +1,3 @@
+#include "SceneShader.h"
+
+using namespace wallpaper;
