catsout/wallpaper-scene-renderer

commit ef0a0652ce000120eef2c1f94f7a64e9b0341f55

add alpha to color4

src/backend_scene/src/WPSceneParser.cpp
@@ -603,8 +603,12 @@ void ParseImageObj(ParseContext& context, wpscene::WPImageObject& img_obj) {
             }
         }
 
-        baseConstSvs["g_Alpha"]      = wpimgobj.alpha;
-        baseConstSvs["g_Color"]      = wpimgobj.color;
+        baseConstSvs["g_Color4"]     = std::array<float, 4> {
+            wpimgobj.color[0],
+            wpimgobj.color[1],
+            wpimgobj.color[2],
+            1.0f
+        };
         baseConstSvs["g_UserAlpha"]  = wpimgobj.alpha;
         baseConstSvs["g_Brightness"] = wpimgobj.brightness;
 

commit f7830a63d583a88896178e2886487f9ff1d7a1e2

Add image alpha to `g_Color4`
The opacity/alpha property isn't passed to shader correctly,
causing some scene not rendering correctly,
e.g. [Starry Sky (2593802083)](https://steamcommunity.com/sharedfiles/filedetails/?id=2593802083)

Closes: #342
Fixes: ef0a065 ("add alpha to color4")
Signed-off-by: fxzjshm <fxzjshm@163.com>

src/backend_scene/src/WPSceneParser.cpp
@@ -607,7 +607,7 @@ void ParseImageObj(ParseContext& context, wpscene::WPImageObject& img_obj) {
             wpimgobj.color[0],
             wpimgobj.color[1],
             wpimgobj.color[2],
-            1.0f
+            wpimgobj.alpha
         };
         baseConstSvs["g_UserAlpha"]  = wpimgobj.alpha;
         baseConstSvs["g_Brightness"] = wpimgobj.brightness;